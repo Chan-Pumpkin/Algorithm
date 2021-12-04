68cc47a45ab307db54aee50a89855220ee7b92c1
Chanho Park <62877858+Chan-Pumpkin@users.noreply.github.com>
2021-12-05 01:41:39 +0900

    Create No10998.java

---
+++ BOJ/No10998.java
@@ -0,0 +1,17 @@
+/*
+10998번 문제
+- 두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+*/
+import java.util.Scanner;
+
+public class No10998 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a*b);
+    }
+}
+/*
+= 정수형 a와 b를 스캐너로 사용자에게 값을 받아, 두 값을 곱해서 출력함.
+*/
