aefdce08188bab4874506322d901c4fa3a2f8e70
Chanho Park <62877858+Chan-Pumpkin@users.noreply.github.com>
2021-12-05 01:05:00 +0900

    Create No1000.java

---
+++ BOJ/No1000.java
@@ -0,0 +1,21 @@
+/*
+1000번 문제
+- 두 수를 입력받고 합을 출력하는 문제
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.println(a+b);
+    }
+}
+/*
+- 정수형 a와 b를 스캐너로 사용자에게 값을 받아 출력할 때, 더해서 출력함.
+*/
