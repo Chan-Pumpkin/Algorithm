93ffecf3c746b84a4c5a096ca858337d1a579826
Chanho Park <62877858+Chan-Pumpkin@users.noreply.github.com>
2021-12-05 01:13:47 +0900

    Update No1000.java

--- BOJ/No1000.java
+++ BOJ/No1000.java
@@ -4,7 +4,7 @@
 */
 import java.util.Scanner;
 
-public class Main {
+public class No1000 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a;
