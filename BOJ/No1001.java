acf7eb3edb81f7adb46ef24fa11ca8764c1aa768
Chanho Park <62877858+Chan-Pumpkin@users.noreply.github.com>
2021-12-05 01:13:33 +0900

    Create No1001.java

---
+++ BOJ/No1001.java
@@ -0,0 +1,17 @@
+/*
+1001번 문제
+- 
+*/
+import java.util.Scanner;
+
+public class No1001 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a-b);
+    }
+}
+/*
+= 정수형 a와 b를 스캐너로 사용자에게 값을 받아, 두 값을 빼서 출력함.
+*/
